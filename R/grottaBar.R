267563bac9a0e7c033bf9dcf7f4f124c2be58886
Andreas Gammelgaard Damsbo <43278243+agdamsbo@users.noreply.github.com>
2023-01-27 11:28:38 +0100

    "n (percentage)" and fontWeight
    new "n (percentage)" numbers format, and added fontWeight parameter

--- R/grottaBar.R
+++ R/grottaBar.R
@@ -24,6 +24,7 @@
 #' @param dir a character indicating if stratified results should be laid out vertically (\code{"v"}) or horizontally \code{"h"}.
 #' @param textSize a number indicating the size of text labels
 #' @param numberSize a number indicating the size of printed numbers
+#' @param textWeight a character string indicating weight of printed numbers. Can be "plain", "bold", "italic" or "bold.italic". 
 #' @param lineSize a number indicating the thickness of lines in the plot
 #' @param returnData a boolean indicating if the data used to create the plot should be returned. For expert users only.
 #'
@@ -86,60 +87,61 @@ grottaBar <- function(x,groupName,scoreName,strataName = NULL,
                       nCol=1, dir="v",
                       width=0.9,
                       textSize=15, numberSize=5,
+                      textWeight="bold",
                       lineSize=0.5,
                       returnData = FALSE
-                      ){
-
+){
+  
   # This code draws heavily from aosmith's answer to the following question:
   # https://stackoverflow.com/questions/51213169/is-there-an-efficient-way-to-draw-lines-between-different-elements-in-a-stacked
-
+  
   # See also this discussion on making ggplot2 play nicely with R CMD CHECK
   # https://stackoverflow.com/questions/9439256/how-can-i-handle-r-cmd-check-no-visible-binding-for-global-variable-notes-when
-
+  
   group <- p_prev <- p <- score <- line_id <- n <- NULL
-
-
-
+  
+  
+  
   x <- as.data.frame(x)
-
+  
   freqName <- setdiff(colnames(x),c(groupName,scoreName,strataName))
   if(length(freqName)>1){
     stop("Too many free columns. Is your data stratified?")
   }
-
+  
   x <- data.frame(strata=x[,strataName],group=x[,groupName],score=x[,scoreName],n=x[,freqName])
-
+  
   # Dummy code strata if it doesn't exist
   if(!("strata" %in% colnames(x))) x <- cbind(strata="",x)
-
+  
   # Get proportions. This has to be done by strata.
   x <- by(x,x$strata,function(x){
     x$p <- x$n
-
+    
     for(i in unique(x$group)) x[x$group == i,"p"] <- x[x$group == i,"p"]/sum(x[x$group == i,"p"])
-
+    
     x <- do.call("rbind",by(x,x$group,function(df){
       df$p_prev <- cumsum(df$p)-df$p
       df
     }))
-
+    
     x
-
+    
   })
   x <- do.call("rbind",x)
   rownames(x) <- NULL
-
+  
   groupLevels <- levels(x$group)
   x$group <- as.numeric(x$group)
-
+  
   # Get dataset for lines. This should also be done by strata
   y <- by(x,x$strata,function(x){
     y <- {}
     for(i in 1:(length(groupLevels)-1)){
       this_y <- x[which(x$group %in% i:(i+1)),]
-
+      
       this_y$score <- as.numeric(this_y$score)
-
+      
       this_y <- rbind(this_y,
                       data.frame(strata=unique(x$strata),
                                  group=c(i,i+1),
@@ -149,17 +151,17 @@ grottaBar <- function(x,groupName,scoreName,strataName = NULL,
                                  p_prev=c(0,0)
                       )
       )
-
+      
       this_y$line_id <- paste(unique(x$strata),this_y$score,i)
-
+      
       this_y$group <- this_y$group - (-1)^(this_y$group==i) * width/2
-
+      
       y <- rbind(y,this_y)
     }
     y
   })
   y <- do.call("rbind",y)
-
+  
   ggp <- ggplot2::ggplot(x)+
     ggplot2::geom_rect(color="black",
                        alpha = ifelse(colourScheme=="grayscale",0.5,1),
@@ -168,69 +170,87 @@ grottaBar <- function(x,groupName,scoreName,strataName = NULL,
                                     ymin=p_prev,ymax=p_prev+p,fill=score))+
     ggplot2::geom_line(data=y, size=lineSize,
                        ggplot2::aes(x=group,y=p+p_prev,group=line_id))
-
-
+  
+  
   if(printNumbers=="count"){
-
-
-
+    
+    
+    
     if(is.integer(x$n)){
       ggp <- ggp+ ggplot2::geom_text(data=x[which(x$n>0),], size=numberSize,
+                                     fontface = textWeight,
                                      ggplot2::aes(x=group,y=p_prev+0.5*p,
                                                   label=sprintf("%d",n)))
     } else {
-
+      
       #Get maximum required decimal places
       maxDecimal <- max(nchar(x$n-floor(x$n))-2)
-
+      
       ggp <- ggp+ ggplot2::geom_text(data=x[which(x$n>0),], size=numberSize,
+                                     fontface = textWeight,
                                      ggplot2::aes(x=group,y=p_prev+0.5*p,
                                                   label=sprintf(sprintf("%%0.%df",maxDecimal),n)))
     }
-
+    
   } else if (printNumbers=="proportion"){
     ggp <- ggp+ ggplot2::geom_text(data=x[which(x$n>0),], size=numberSize,
+                                   fontface = textWeight,
                                    ggplot2::aes(x=group,y=p_prev+0.5*p,
                                                 label=sprintf("%0.2f",p)))
   } else if (printNumbers=="percentage"){
     ggp <- ggp+ ggplot2::geom_text(data=x[which(x$n>0),], size=numberSize,
+                                   fontface = textWeight,
                                    ggplot2::aes(x=group,y=p_prev+0.5*p,
                                                 label=sprintf("%2.2f",100*p)))
+  } else if (printNumbers=="n (percentage)") {
+    
+    if(is.integer(x$n)){
+      ggp <- ggp+ ggplot2::geom_text(data=x[which(x$n>0),], size=numberSize,
+                                     fontface = textWeight,
+                                     ggplot2::aes(x=group,y=p_prev+0.5*p,
+                                                  label=sprintf("%d\n(%2.1f%s)",n,100*p,"%")))
+    } else {
+      stop("n (percentage) works only with integers")
+    }
+    
+    
+    
+    
   } else if (printNumbers == "none"){
-
+    
     # Do nothing if we were told not to print any numbers
-
+    
   } else {
     stop("Unrecognised option for printNumbers")
   }
-
+  
   if(colourScheme=="lowGreen"){
-
+    
     ggp <- ggp + ggplot2::scale_fill_brewer(palette="RdYlGn",direction = -1)
-
+    
   } else if(colourScheme=="lowRed"){
-
+    
     ggp <- ggp + ggplot2::scale_fill_brewer(palette="RdYlGn",direction = 1)
-
+    
   } else if  (colourScheme=="grayscale"){
-
+    
     ggp <- ggp + ggplot2::scale_fill_brewer(palette="Greys")
-
+    
   } else if ( colourScheme =="custom"){
-
+    
     # Do nothing, assume the user will handle this later.
-
+    
   } else {
-
+    
     stop("colourScheme not recognised")
-
+    
   }
-
-
+  
+  
   if(!is.null(strataName)){
     ggp <- ggp+ggplot2::facet_wrap(~strata, ncol = nCol, dir = dir)
   }
-
+  
   ggp <- ggp +
     ggplot2::scale_x_continuous(breaks = 1:length(groupLevels), labels=groupLevels)+
     ggplot2::scale_y_continuous(labels=scales::percent_format(),expand = ggplot2::expansion(add=0.01))+
@@ -245,10 +265,10 @@ grottaBar <- function(x,groupName,scoreName,strataName = NULL,
                    panel.grid.minor = ggplot2::element_blank(),
                    text = ggplot2::element_text(size=textSize),
                    plot.margin = ggplot2::margin(1, 1, 1, 1, "cm")
-                  )
-
+    )
+  
   ggp
-
+  
   if(returnData)
   {
     out <- list(plot=ggp, rectData=x, lineData=y)
@@ -258,7 +278,7 @@ grottaBar <- function(x,groupName,scoreName,strataName = NULL,
   {
     return(ggp)
   }
-
+  
 }
 
 
